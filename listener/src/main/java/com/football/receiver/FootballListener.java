e00850eac934cb06a423e4ef12e256b54f3db7be
Anna Zhukovec <a.zhukovec1901@gmail.com>
2018-12-11 01:56:44 +0300

    docker listener

--- listener/src/main/java/com/football/receiver/FootballListener.java
+++ listener/src/main/java/com/football/receiver/FootballListener.java
@@ -3,6 +3,8 @@ package com.football.receiver;
 import com.football.domain.FootballPlayer;
 import com.football.domain.MoneyRequest;
 import com.football.service.FootballService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +12,8 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class FootballListener {
+
+    private final static Logger logger = LoggerFactory.getLogger(FootballListener.class);
 
     @Autowired
     private FootballService footballService;
@@ -20,6 +24,7 @@ public class FootballListener {
     @RabbitListener(queues = "shot")
     public void makeShot(FootballPlayer footballPlayer) {
         MoneyRequest moneyRequest = footballService.makeShot(footballPlayer);
+        logger.info("shot result - ", moneyRequest.getMoney());
         rabbitTemplate.convertAndSend("money", moneyRequest);
     }
 }
